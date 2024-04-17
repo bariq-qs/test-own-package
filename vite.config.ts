2a144d42f3ef0c51f680bdf42fbb4dbce3ad53ca
Bariq Qushoyyi <bariqqushoyyi@gmail.com>
2024-04-17 10:32:40 +0700

    add config to built generic types

--- vite.config.ts
+++ vite.config.ts
@@ -50,4 +50,10 @@ export default defineConfig({
       },
     },
   },
+  // server: {
+  //   fs: {
+  //     // Allow serving files from one level up to the project root
+  //     allow: ['..'],
+  //   },
+  // },
 })
